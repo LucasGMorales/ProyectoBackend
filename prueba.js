03ce8d1dce4c018290cf0f43da6ec4149c443077
Lucas Gabriel Morales <lucas_gabriel_morales@outlook.com>
2023-12-28 20:10:22 -0300

    Proyecto Backend entrega 2, Act.Testeo

--- prueba.js
+++ prueba.js
@@ -2,6 +2,7 @@ const ProductManager = require('./clases');
 
 const productManager = new ProductManager(); 
 
+
 // agregar productos
 productManager.addProduct({
     title: "harina 0000",
@@ -23,12 +24,42 @@ productManager.addProduct({
 
 console.log("Lista de productos:");
 
-// buscar producto x ID
+// buscar producto x ID  --- Este id se utiliza tambien para update y deleat, hay que comentar el codigo abajo para que no cree y elimine el mismo producto seleccionado,
+// esta pensado para testear una cosa a la vez
+// si no se modifica el codigo, crea y elimina el id 6, quedando el 7 en el array de productos.json
 const encontrarIdProducto = 6;
 const foundProducto = productManager.getProductById(encontrarIdProducto);
 
+
+// getproducts
+console.log(productManager.getProducts());
+
+// updateproduct
+const productoActualizado = {
+    title: "producto nombre",
+    description: "descripcion",
+    price: 300,
+    thumbnail: "imagen",
+    code: "000",
+    stock: 25,
+    id: encontrarIdProducto 
+};
+productManager.updateProduct(encontrarIdProducto, productoActualizado);
+console.log(productManager.getProductById(encontrarIdProducto));
+
+// deleteproduct
+const idAEliminar = encontrarIdProducto;
+const productoEliminado = productManager.deleteProduct(idAEliminar);
+
+if (productoEliminado) {
+    console.log(`el producto ${idAEliminar} fue eliminado.`);
+} else {
+    console.log(`Error 404: prodcuto ${idAEliminar} no encontrado.`);
+}
+
+// code
 if (foundProducto) {
     console.log(`Producto code: ${encontrarIdProducto}:`, foundProducto);
 } else {
-    console.log(`Producto code: ${encontrarIdProducto}.`);
+    console.log(`Error! Producto code: ${encontrarIdProducto}.`);
 }
